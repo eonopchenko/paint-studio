9eaf82c2ec2906a2ddda4841d109ab23cd6ded35
eonopchenko <jake.spb@gmail.com>
2017-08-20 19:49:02 +1200

    ~Refactored

--- app/src/main/java/nz/ac/unitec/paintstudio/CustomView.java
+++ app/src/main/java/nz/ac/unitec/paintstudio/CustomView.java
@@ -21,49 +21,80 @@ public class CustomView extends View implements View.OnTouchListener {
         ELLIPSE
     }
 
-    private class SketchSettings {
+    private class Sketch {
 
-        private int mStrokeColor;
-        private int mFillColor;
+        protected int mStrokeColor;
 
-        SketchSettings(int strokeColor, int fillColor) {
-            mStrokeColor = strokeColor;
-            mFillColor = fillColor;
-        }
-
-        SketchSettings(SketchSettings sketchSettings) {
-            this.mStrokeColor = sketchSettings.getStrokeColor();
-            this.mFillColor = sketchSettings.getFillColor();
-        }
+        protected int mStrokeWidth;
 
         public int getStrokeColor() {
             return mStrokeColor;
         }
 
-        public void setStrokeColor(int strokeColor) {
-            mStrokeColor = strokeColor;
+        public int getStrokeWidth() {
+            return mStrokeWidth;
         }
+    }
+
+    private class PathSketch extends Sketch {
+
+        private Path mPath;
+
+        PathSketch(int strokeColor, int strokeWidth) {
+            mPath = new Path();
+            this.mStrokeColor = strokeColor;
+            this.mStrokeWidth = strokeWidth;
+        }
+
+        public Path getPath() {
+            return mPath;
+        }
+    }
+
+    private class EllipseSketch extends Sketch {
+
+        private int mFillColor;
+        private RectF mRectF;
+        private float mCenterX, mCenterY;
 
         public int getFillColor() {
             return mFillColor;
         }
 
-        public void setFillColor(int fillColor) {
-            mFillColor = fillColor;
+        EllipseSketch(float centerX, float centerY, int strokeColor, int strokeWidth, int fillColor) {
+            this.mRectF = new RectF();
+            this.mCenterX = centerX;
+            this.mCenterY = centerY;
+            this.mStrokeColor = strokeColor;
+            this.mStrokeWidth = strokeWidth;
+            this.mFillColor = fillColor;
+        }
+
+        public RectF getRectF() {
+            return mRectF;
+        }
+
+        public float getCenterX() {
+            return mCenterX;
+        }
+
+        public float getCenterY() {
+            return mCenterY;
         }
     }
 
-    private Paint mDrawPaint;
-    private SketchSettings mSketchSettings;
-    private Path mDrawPath;
-    private RectF mDrawOval;
+    private Paint mPaint;
     private Tool mTool = Tool.BRUSH;
-    private float mCenterX, mCenterY;
 
-    private ArrayList<Object> mSketchList = new ArrayList<>();
-    private ArrayList<Object> mUndoneSketchList = new ArrayList<>();
-    private ArrayList<SketchSettings> mSketchSettingsList = new ArrayList<>();
-    private ArrayList<SketchSettings> mUndoneSketchSettingsList = new ArrayList<>();
+    private PathSketch mCurrentPath;
+    private EllipseSketch mCurrentEllipse;
+
+    private ArrayList<Sketch> mSketchList = new ArrayList<>();
+    private ArrayList<Sketch> mUndoneSketchList = new ArrayList<>();
+
+    private int mStrokeWidth;
+    private int mStrokeColor;
+    private int mFillColor;
 
     public CustomView(Context context) {
         super(context);
@@ -80,42 +111,33 @@ public class CustomView extends View implements View.OnTouchListener {
         init();
     }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-    }
-
     private void init() {
 
-        mDrawPath = new Path();
-        mDrawOval = new RectF();
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
 
-        mDrawPaint = new Paint();
+        mStrokeWidth = 10;
+        mStrokeColor = Color.RED;
+        mFillColor = Color.YELLOW;
 
-        mDrawPaint.setAntiAlias(true);
-        mDrawPaint.setStrokeWidth(20);
-        mDrawPaint.setStyle(Paint.Style.STROKE);
-        mDrawPaint.setStrokeJoin(Paint.Join.ROUND);
-        mDrawPaint.setStrokeCap(Paint.Cap.ROUND);
-
-        mSketchSettings = new SketchSettings(Color.YELLOW, Color.GREEN);
+        mCurrentPath = new PathSketch(mStrokeColor, mStrokeWidth);
+        mCurrentEllipse = new EllipseSketch(0, 0, mStrokeColor, mStrokeWidth, mFillColor);
 
         setOnTouchListener(this);
     }
 
+
+    ///--- INTERFACES ---///
+
     public void Undo() {
 
-        int pathsSize = mSketchList.size();
-        int colorsSize = mSketchSettingsList.size();
+        int size = mSketchList.size();
 
-        if (pathsSize > 0) {
-            mUndoneSketchList.add(mSketchList.get(pathsSize - 1));
-            mSketchList.remove(pathsSize - 1);
-        }
-
-        if (colorsSize > 0) {
-            mUndoneSketchSettingsList.add(mSketchSettingsList.get(colorsSize - 1));
-            mSketchSettingsList.remove(colorsSize - 1);
+        if (size > 0) {
+            mUndoneSketchList.add(mSketchList.get(size - 1));
+            mSketchList.remove(size - 1);
         }
 
         invalidate();
@@ -123,17 +145,11 @@ public class CustomView extends View implements View.OnTouchListener {
 
     public void Redo() {
 
-        int pathsSize = mUndoneSketchList.size();
-        int colorsSize = mUndoneSketchSettingsList.size();
+        int size = mUndoneSketchList.size();
 
-        if (pathsSize > 0) {
-            mSketchList.add(mUndoneSketchList.get(pathsSize - 1));
-            mUndoneSketchList.remove(pathsSize - 1);
-        }
-
-        if(colorsSize > 0) {
-            mSketchSettingsList.add(mUndoneSketchSettingsList.get(colorsSize - 1));
-            mUndoneSketchSettingsList.remove(colorsSize - 1);
+        if (size > 0) {
+            mSketchList.add(mUndoneSketchList.get(size - 1));
+            mUndoneSketchList.remove(size - 1);
         }
 
         invalidate();
@@ -143,81 +159,64 @@ public class CustomView extends View implements View.OnTouchListener {
         this.mTool = tool;
     }
 
+
+    ///--- OVERRIDDEN METHODS ---///
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
+
         super.onDraw(canvas);
 
-        for (int index = 0; index < mSketchList.size(); index++) {
-
-            Object o = mSketchList.get(index);
-            SketchSettings sketchSettings = mSketchSettingsList.get(index);
-            mDrawPaint.setColor(sketchSettings.getStrokeColor());
-            mDrawPaint.setStyle(Paint.Style.STROKE);
-
-            /// Path
-            if(o instanceof Path) {
-                canvas.drawPath((Path) o, mDrawPaint);
-
-            /// Ellipse
-            } else if(o instanceof RectF) {
-                canvas.drawOval((RectF) o, mDrawPaint);
-                mDrawPaint.setColor(sketchSettings.getFillColor());
-                mDrawPaint.setStyle(Paint.Style.FILL);
-                canvas.drawOval((RectF) o, mDrawPaint);
+        for(Sketch s : mSketchList) {
+            mPaint.setColor(s.getStrokeColor());
+            mPaint.setStrokeWidth(s.getStrokeWidth());
+            mPaint.setStyle(Paint.Style.STROKE);
+            if (s instanceof PathSketch) {
+                canvas.drawPath(((PathSketch) s).getPath(), mPaint);
+            } else if(s instanceof EllipseSketch) {
+                RectF rectF = ((EllipseSketch) s).getRectF();
+                canvas.drawOval(rectF, mPaint);
+                mPaint.setColor(((EllipseSketch) s).getFillColor());
+                mPaint.setStyle(Paint.Style.FILL);
+                canvas.drawOval(rectF, mPaint);
             }
         }
 
-        mDrawPaint.setColor(mSketchSettings.getStrokeColor());
-        mDrawPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(mStrokeColor);
+        mPaint.setStrokeWidth(mStrokeWidth);
+        mPaint.setStyle(Paint.Style.STROKE);
         if(mTool == Tool.BRUSH) {
-            canvas.drawPath(mDrawPath, mDrawPaint);
+            if(mCurrentPath != null) {
+                canvas.drawPath(mCurrentPath.getPath(), mPaint);
+            }
         } else if(mTool == Tool.ELLIPSE) {
-            canvas.drawOval(mDrawOval, mDrawPaint);
-            mDrawPaint.setColor(mSketchSettings.getFillColor());
-            mDrawPaint.setStyle(Paint.Style.FILL);
-            canvas.drawOval(mDrawOval, mDrawPaint);
+            if(mCurrentEllipse != null) {
+                canvas.drawOval(mCurrentEllipse.getRectF(), mPaint);
+                mPaint.setColor(mFillColor);
+                mPaint.setStyle(Paint.Style.FILL);
+                canvas.drawOval(mCurrentEllipse.getRectF(), mPaint);
+            }
         }
-    }
-
-    public void setSketchStack(ArrayList<Object> mSketchStack) {
-        this.mSketchList = mSketchStack;
-    }
-
-    public void setUndoneSketchStack(ArrayList<Object> mUndoneSketchStack) {
-        this.mUndoneSketchList = mUndoneSketchStack;
-    }
-
-    public void setColorStack(ArrayList<SketchSettings> mColorStack) {
-        this.mSketchSettingsList = mColorStack;
-    }
-
-    public void setUndoneColorStack(ArrayList<SketchSettings> mUndoneColorStack) {
-        this.mUndoneSketchSettingsList = mUndoneColorStack;
     }
 
     @Override
     public Parcelable onSaveInstanceState() {
         Parcelable superState = super.onSaveInstanceState();
         SavedContext savedContext = new SavedContext(superState);
-
         savedContext.sketches = mSketchList;
-        savedContext.undoneSketches = mUndoneSketchList;
-        savedContext.sketchSettings = mSketchSettingsList;
-        savedContext.undoneSketchSettings = mUndoneSketchSettingsList;
-
         return savedContext;
     }
 
     @Override
     public void onRestoreInstanceState(Parcelable state) {
-
         SavedContext savedContext = (SavedContext) state;
         super.onRestoreInstanceState(savedContext.getSuperState());
-
-        setSketchStack(savedContext.sketches);
-        setUndoneSketchStack(savedContext.undoneSketches);
-        setColorStack(savedContext.sketchSettings);
-        setUndoneColorStack(savedContext.undoneSketchSettings);
+        this.mSketchList = savedContext.sketches;
     }
 
     @Override
@@ -229,33 +228,33 @@ public class CustomView extends View implements View.OnTouchListener {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 if(mTool == Tool.BRUSH) {
-                    mDrawPath.moveTo(touchX, touchY);
+                    mCurrentPath = new PathSketch(mStrokeColor, mStrokeWidth);
+                    mCurrentPath.getPath().moveTo(touchX, touchY);
                 } else if (mTool == Tool.ELLIPSE) {
-                    mCenterX = touchX;
-                    mCenterY = touchY;
-                    mDrawOval.set(mCenterX, mCenterY, mCenterX, mCenterY);
+                    mCurrentEllipse = new EllipseSketch(touchX, touchY, mStrokeColor, mStrokeWidth, mFillColor);
                 }
                 break;
             case MotionEvent.ACTION_MOVE:
                 if(mTool == Tool.BRUSH) {
-                    mDrawPath.lineTo(touchX, touchY);
+                    mCurrentPath.getPath().lineTo(touchX, touchY);
                 } else if (mTool == Tool.ELLIPSE) {
-                    float width = touchX - mCenterX;
-                    float height = touchY - mCenterY;
+                    float centerX = mCurrentEllipse.getCenterX();
+                    float centerY = mCurrentEllipse.getCenterY();
+                    float width = touchX - centerX;
+                    float height = touchY - centerY;
                     width = width < 0 ? -width : width;
                     height = height < 0 ? -height : height;
-                    mDrawOval.set(mCenterX - width, mCenterY - height, mCenterX + width, mCenterY + height);
+                    mCurrentEllipse.getRectF().set(centerX - width, centerY - height, centerX + width, centerY + height);
                 }
                 break;
             case MotionEvent.ACTION_UP:
                 if(mTool == Tool.BRUSH) {
-                    mSketchList.add(new Path(mDrawPath));
-                    mDrawPath.reset();
+                    mSketchList.add(mCurrentPath);
+                    mCurrentPath = null;
                 } else if (mTool == Tool.ELLIPSE) {
-                    mSketchList.add(new RectF(mDrawOval));
-                    mDrawOval.set(0, 0, 0, 0);
+                    mSketchList.add(mCurrentEllipse);
+                    mCurrentEllipse = null;
                 }
-                mSketchSettingsList.add(new SketchSettings(mSketchSettings));
                 mUndoneSketchList.clear();
                 break;
             default:
@@ -266,11 +265,10 @@ public class CustomView extends View implements View.OnTouchListener {
         return true;
     }
 
+    ///--- SAVE CONTEXT (STATIC) ---///
     static class SavedContext extends BaseSavedState {
-        ArrayList<Object> sketches;
-        ArrayList<Object> undoneSketches;
-        ArrayList<SketchSettings> sketchSettings;
-        ArrayList<SketchSettings> undoneSketchSettings;
+
+        ArrayList<Sketch> sketches;
 
         SavedContext(Parcelable superState) {
             super(superState);
@@ -279,18 +277,12 @@ public class CustomView extends View implements View.OnTouchListener {
         private SavedContext(Parcel in) {
             super(in);
             in.readList(sketches, sketches.getClass().getClassLoader());
-            in.readList(undoneSketches, undoneSketches.getClass().getClassLoader());
-            in.readList(sketchSettings, sketchSettings.getClass().getClassLoader());
-            in.readList(undoneSketchSettings, undoneSketchSettings.getClass().getClassLoader());
         }
 
         @Override
         public void writeToParcel(Parcel out, int flags) {
             super.writeToParcel(out, flags);
             out.writeList(sketches);
-            out.writeList(undoneSketches);
-            out.writeList(sketchSettings);
-            out.writeList(undoneSketchSettings);
         }
 
         public static final Parcelable.Creator<SavedContext> CREATOR
